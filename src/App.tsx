ebaaecf7eb386fa42e3f043398bb10c3e74aa6f0
wheesnoza <45123151+wheesnoza@users.noreply.github.com>
2022-09-19 13:55:47 +0900

    Use container from mui.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Container, LinearProgress } from '@mui/material';
 import { Suspense, lazy } from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
@@ -10,19 +11,21 @@ const Home = lazy(() => import('./pages/Home/Home'));
 
 function App() {
   return (
-    <Suspense fallback={<h1>Loading...</h1>}>
+    <Suspense fallback={<LinearProgress />}>
       <Provider store={store}>
-        <BrowserRouter>
-          <Routes>
-            <Route element={<GuestGuard />}>
-              <Route path={PublicRoutes.LOGIN} element={<Login />} />
-            </Route>
-            <Route element={<AuthGuard />}>
-              <Route path={PrivateRoutes.HOME} element={<Home />} />
-            </Route>
-            <Route path="*" element={<>NOT FOUND</>} />
-          </Routes>
-        </BrowserRouter>
+        <Container maxWidth="lg">
+          <BrowserRouter>
+            <Routes>
+              <Route element={<GuestGuard />}>
+                <Route path={PublicRoutes.LOGIN} element={<Login />} />
+              </Route>
+              <Route element={<AuthGuard />}>
+                <Route path={PrivateRoutes.HOME} element={<Home />} />
+              </Route>
+              <Route path="*" element={<>NOT FOUND</>} />
+            </Routes>
+          </BrowserRouter>
+        </Container>
       </Provider>
     </Suspense>
   );

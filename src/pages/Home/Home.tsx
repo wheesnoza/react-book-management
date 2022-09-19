5529ba0f375f42d30fd2e823289b3c92f6bee1e3
wheesnoza <45123151+wheesnoza@users.noreply.github.com>
2022-09-19 13:55:27 +0900

    Add logout button.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,15 +1,20 @@
-import { Logout } from '@/components';
+import { resetUser } from '@/redux/states/user';
 import { AppStore } from '@/redux/store';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 export const Home = () => {
+  const dispatch = useDispatch();
   const user = useSelector((store: AppStore) => store.user);
+
+  const handleClick = () => {
+    dispatch(resetUser());
+  };
 
   return (
     <>
       <h1>Home</h1>
-      <p>{JSON.stringify(user)}</p>
-      <Logout />
+      <span>{JSON.stringify(user)}</span>
+      <button onClick={handleClick}>Logout</button>
     </>
   );
 };

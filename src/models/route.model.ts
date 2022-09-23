aa0f12c990f6ab984569b7c2e767a5955a11d19e
wheesnoza <45123151+wheesnoza@users.noreply.github.com>
2022-09-23 20:41:13 +0900

    Append routes.

--- src/models/route.model.ts
+++ src/models/route.model.ts
@@ -5,7 +5,8 @@ export enum PublicRoutes {
 export enum PrivateRoutes {
   HOME = '/',
   BOOK_DETAIL = '/books/:bookId',
+  BOOK_CREATE = '/books/new',
   BOOK_LEND_PETITION = '/books/:bookId/lend/petition',
   BOOK_PROCURE_PETITION = '/books/procure/petition',
-  APPLICATIONS = '/applications'
+  APPLICATIONS = '/applications',
 }

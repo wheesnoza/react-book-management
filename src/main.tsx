d803836757869d7991d830d69676b023355668bb
wheesnoza <45123151+wheesnoza@users.noreply.github.com>
2022-09-24 10:33:58 +0900

    Remove strict.

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
 import { I18nextProvider } from 'react-i18next';
-import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './Reset.css';
 import i18n from './locales/i18n';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <StrictMode>
-    <I18nextProvider i18n={i18n}>
-      <App />
-    </I18nextProvider>
-  </StrictMode>
+  <I18nextProvider i18n={i18n}>
+    <App />
+  </I18nextProvider>
 );

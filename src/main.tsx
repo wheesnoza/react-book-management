4d3496ada2480bc3a7656a79044e6df96a810f8e
wheesnoza <45123151+wheesnoza@users.noreply.github.com>
2022-09-24 21:29:36 +0900

    Run worker only when environment is development.

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './Reset.css';
 import i18n from './locales/i18n';
+import { worker } from './mocks/browser';
+
+if (process.env.NODE_ENV === 'development') {
+  worker.start();
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <I18nextProvider i18n={i18n}>

541f8d93692a289ae4b7b5bae28f8ba5706b8bf5
wheesnoza <45123151+wheesnoza@users.noreply.github.com>
2022-09-24 22:37:42 +0900

    Implement adapters to parse, modify, secure, server data.

--- src/adapters/book.adapter.ts
+++ src/adapters/book.adapter.ts
@@ -1,12 +1,13 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Book } from '@/models';
 
 export const bookAdapter = (data: any): Book => ({
-  id: data.id,
-  title: data.title,
-  description: data.description,
-  stock: data.stock,
-  status: data.status,
-  thumbnail: data.thumbnail,
+  id: data?.id,
+  title: data?.title,
+  description: data?.description,
+  stock: data?.stock,
+  status: data?.status,
+  thumbnail: data?.thumbnail,
 });
 
 export default bookAdapter;
--- src/adapters/lend.adapter.ts
+++ src/adapters/lend.adapter.ts
@@ -1,13 +1,14 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Lend } from '@/models';
 
 export const lendAdapter = (data: any): Lend => ({
-  from: new Date(data.from),
-  to: new Date(data.to),
+  from: new Date(data?.from),
+  to: new Date(data?.to),
   user: {
-    id: data.user.id,
-    name: data.user.name,
-    email: data.user.email,
-    role: data.user.role,
+    id: data?.user.id,
+    name: data?.user.name,
+    email: data?.user.email,
+    role: data?.user.role,
   },
 });
 

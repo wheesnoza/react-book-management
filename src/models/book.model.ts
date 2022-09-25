3f9bdd87f86f3be1d53c416f07a690b723877a01
wheesnoza <45123151+wheesnoza@users.noreply.github.com>
2022-09-25 22:31:54 +0900

    Refact the validation shcema of book by moving it to book model.

--- src/models/book.model.ts
+++ src/models/book.model.ts
@@ -1,3 +1,6 @@
+import { t } from 'i18next';
+import yup from '@/locales/yup.locale';
+
 export enum BookStatus {
   AVAILABLE = 'available',
   UNAVAILABLE = 'unavailable',
@@ -11,3 +14,14 @@ export interface Book {
   status: BookStatus;
   thumbnail: string;
 }
+
+export const bookSchema = yup.object().shape({
+  title: yup.string().label(t('book.title')).required().max(80),
+  description: yup.string().label(t('book.description')).required().max(250),
+  stock: yup.number().label(t('book.stock')).required().min(1),
+  status: yup
+    .string()
+    .oneOf(Object.values(BookStatus))
+    .label(t('book.status'))
+    .required(),
+});
--- src/pages/Book/Create/BookCreateLogic.tsx
+++ src/pages/Book/Create/BookCreateLogic.tsx
@@ -3,20 +3,8 @@ import { t } from 'i18next';
 import { useForm } from 'react-hook-form';
 import { generatePath, useNavigate } from 'react-router-dom';
 import { alert } from '@/services/alert.service';
-import { AlertLevel, Book, BookStatus, PrivateRoutes } from '@/models';
-import yup from '@/locales/yup.locale';
+import { AlertLevel, Book, bookSchema, PrivateRoutes } from '@/models';
 import BookForm from '../Form/BookForm';
-
-const schema = yup.object().shape({
-  title: yup.string().label(t('book.title')).required().max(80),
-  description: yup.string().label(t('book.description')).required().max(250),
-  stock: yup.number().label(t('book.stock')).required().min(1),
-  status: yup
-    .string()
-    .oneOf(Object.values(BookStatus))
-    .label(t('book.status'))
-    .required(),
-});
 
 interface Props {
   defaultValues: Book;
@@ -28,7 +16,7 @@ export const BookCreateLogic = ({ defaultValues, onSubmit }: Props) => {
   const form = useForm<Book>({
     mode: 'onSubmit',
     defaultValues,
-    resolver: yupResolver(schema),
+    resolver: yupResolver(bookSchema),
   });
 
   const handleSubmit = async (book: Book) => {

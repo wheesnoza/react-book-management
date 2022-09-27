cc7ebcb767bcd80a6cf075a703570226447ba315
wheesnoza <45123151+wheesnoza@users.noreply.github.com>
2022-09-27 19:34:22 +0900

    Add procure create path into route model.

--- src/models/route.model.ts
+++ src/models/route.model.ts
@@ -9,5 +9,6 @@ export enum PrivateRoutes {
   BOOK_EDIT = '/books/:bookId/edit',
   BOOK_LEND_PETITION = '/books/:bookId/lend/petition',
   BOOK_PROCURE_PETITION = '/books/procure/petition',
+  BOOK_PROCURE_DETAIL = '/books/procure/:procureId',
   APPLICATIONS = '/applications',
 }

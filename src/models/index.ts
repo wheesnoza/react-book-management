4354fa2b90b4c7e001656191e0ecf4dfa1c25321
wheesnoza <45123151+wheesnoza@users.noreply.github.com>
2022-09-27 19:32:39 +0900

    Update model directory index.

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,6 +1,7 @@
 export * from './alert.model';
 export * from './book.model';
 export * from './lend.model';
+export * from './procure.model';
 export * from './role.model';
 export * from './route.model';
 export * from './user.model';

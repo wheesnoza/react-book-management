afd817230dea24e4da66ccfdae86b08bd7e30ce7
wheesnoza <45123151+wheesnoza@users.noreply.github.com>
2022-09-27 19:40:34 +0900

    Permit any.

--- src/utilities/enum.utility.ts
+++ src/utilities/enum.utility.ts
@@ -1,5 +1,6 @@
 import { values as lodashValues, filter } from 'lodash';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const onlyNumericValues = (enumObject: any): any[] => {
   return filter(lodashValues(enumObject), (value) => {
     return typeof value === 'number';

cae87bc52e50c5710e736be0854daae6958adbd3
wheesnoza <45123151+wheesnoza@users.noreply.github.com>
2022-09-29 15:27:37 +0900

    Implement lend models and languages.

---
+++ src/locales/ja/lend.ja.ts
@@ -0,0 +1,10 @@
+export const lendTranslations = {
+  range: '借りる期間',
+  petition: {
+    created: '記録しました - 本の持ち出しができます',
+    create: 'この本を借りる',
+    receivingMethod: '受け取り方法',
+    0: '郵送',
+    1: '自分で持ち出す',
+  },
+};
--- src/models/lend.model.ts
+++ src/models/lend.model.ts
@@ -1,7 +1,23 @@
+import yup from '@/locales/yup.locale';
+import { onlyNumericValues } from '@/utilities';
+import { Book } from './book.model';
 import { User } from './user.model';
 
+export enum LendReceivingMethod {
+  SEND_BY_POST,
+  GET_IT_BY_SELF,
+}
+
 export interface Lend {
+  id: string;
   from: Date;
   to: Date;
+  book: Book;
   user: User;
+  receivingMethod: LendReceivingMethod;
 }
+
+export const lendSchema = yup.object().shape({
+  range: yup.array().of(yup.date()).length(2),
+  receivingMethod: yup.number().oneOf(onlyNumericValues(LendReceivingMethod)),
+});

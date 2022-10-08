b60c9fee1c9c2cfcf72a1e56afeb4229a9e5dfa9
wheesnoza <45123151+wheesnoza@users.noreply.github.com>
2022-10-08 14:49:00 +0900

    Append data to mui datagrid.

--- src/pages/Application/List/ApplicationList.tsx
+++ src/pages/Application/List/ApplicationList.tsx
@@ -1,7 +1,7 @@
 import { Box } from '@mui/material';
 import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
-import { useEffect, useState } from 'react';
-import { alert } from '@/services';
+import { useState } from 'react';
+import useSWR from 'swr';
 
 const columns: GridColDef[] = [
   { field: 'id', headerName: 'ID', width: 90 },
@@ -15,25 +15,27 @@ const columns: GridColDef[] = [
   { field: 'applied_at', headerName: '申請日時', width: 200 },
 ];
 
+const PAGE_SIZE = 5;
 export const ApplicationList = () => {
-  const [applications, setApplications] = useState([]);
-
-  useEffect(() => {
-    fetch('/api/applications')
-      .then((data) => data.json())
-      .then(setApplications)
-      .catch(() => {
-        alert.display('Somethingw was wrong');
-      });
-  }, []);
+  const [pageIndex, setPageIndex] = useState(0);
+  const { data: applications } = useSWR(
+    `/api/applications?page=${pageIndex + 1}&limit=${PAGE_SIZE}`,
+    {
+      suspense: false,
+    }
+  );
 
   return (
     <Box sx={{ height: 400, width: '100%' }}>
       <DataGrid
-        rows={applications}
+        loading={!applications}
+        rows={applications ?? []}
         columns={columns}
-        pageSize={10}
-        rowsPerPageOptions={[10]}
+        pageSize={PAGE_SIZE}
+        paginationMode="server"
+        rowCount={15}
+        onPageChange={setPageIndex}
+        page={pageIndex}
       />
     </Box>
   );
